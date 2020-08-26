422826075e032aab4a5f7c91866caefc4f5609ec
kbrv37 <Al.pizano@motorolasolutions.com>
2020-08-26 07:40:44 -0500

    filter ex

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -14,7 +14,11 @@ public class App
         // map, filter etc
         Observable.just("Hello world.").subscribe(line -> System.out.println(line));
 
+        // map
         Observable.range(0,5).map(index -> "Hello World....").subscribe(line -> System.out.println(line));
+
+        // filter
+        Observable.just(1, 3, 4, 7, 10, 12, 13, 11, 15).filter(val -> val % 2 == 0).subscribe(line -> System.out.println(line));
 
     }
 }
